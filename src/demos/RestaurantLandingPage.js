2eb89a8621ba5b9269cd5d470553d6dba1e3896f
Ignacio Monzalvo <ignaciomonzalvo@Ignacios-MacBook-Pro.local>
2023-01-05 15:22:29 -0300

    First commit

---
+++ src/demos/RestaurantLandingPage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import Hero from "components/hero/TwoColumnWithVideo.js";
+import Features from "components/features/ThreeColSimple.js";
+import MainFeature from "components/features/TwoColWithButton.js";
+import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
+import TabGrid from "components/cards/TabCardGrid.js";
+import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
+import DownloadApp from "components/cta/DownloadApp.js";
+import Footer from "components/footers/FiveColumnWithInputForm.js";
+
+import chefIconImageSrc from "images/chef-icon.svg";
+import celebrationIconImageSrc from "images/celebration-icon.svg";
+import shopIconImageSrc from "images/shop-icon.svg";
+
+export default () => {
+  const Subheading = tw.span`tracking-wider text-sm font-medium`;
+  const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
+  const HighlightedTextInverse = tw.span`bg-gray-100 text-primary-500 px-4 transform -skew-x-12 inline-block`;
+  const Description = tw.span`inline-block mt-8`;
+  const imageCss = tw`rounded-4xl h-128`;
+  return (
+    <AnimationRevealPage>
+      <Hero
+        heading={<>Delicious & Affordable <HighlightedText>Meals Near You.</HighlightedText></>}
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        imageSrc={require('../images/demo/image2.jpeg')}
+        imageCss={imageCss}
+        imageDecoratorBlob={true}
+        primaryButtonText="Order Now"
+        watchVideoButtonText="Meet The Chefs"
+      />
+      <TabGrid
+        heading={
+          <>
+            Nuestros <HighlightedText>productos.</HighlightedText>
+          </>
+        }
+      />
+    </AnimationRevealPage>
+  );
+}
